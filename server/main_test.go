a8e6be9d04d2fbb1ff9d80e95114d4cca5a0c912
mauleyzaola <ismael.martinez.a@gmail.com>
2021-03-03 16:29:11 -0600

    GSS-05 Implement unit test

---
+++ server/main_test.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestPostGoSecret(t *testing.T) {
+	tt := []struct {
+		name    string
+		value   string
+		status  int
+		wantErr bool
+	}{
+		{name: "", value: `{"value": "hola", "unit": "m", "utime":2}`, status: http.StatusOK, wantErr: false},
+		{name: "missing value", value: "", status: http.StatusBadRequest, wantErr: true},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			req, err := http.NewRequest(http.MethodPost, "localhost:8081/secret", strings.NewReader(tc.value))
+			if err != nil {
+				t.Fatalf("could not created request: %v", err)
+			}
+
+			rec := httptest.NewRecorder()
+			PostGoSecret(rec, req)
+
+			res := rec.Result()
+			defer res.Body.Close()
+
+			if tc.wantErr {
+				if res.StatusCode != http.StatusBadRequest {
+					t.Errorf("expected status badrequest; got %v", res.Status)
+				}
+			} else {
+				if res.StatusCode != http.StatusOK {
+					t.Errorf("expected status ok; got %v", res.Status)
+				}
+				b, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					t.Fatalf("could not read response: %v", err)
+				}
+				r := string(bytes.TrimSpace(b))
+				if r == "" {
+					t.Fatalf("there is no value: %s", r)
+				}
+			}
+
+		})
+
+	}
+}
+
+func TestGetGoSecret(t *testing.T) {
+	tt := []struct {
+		name    string
+		value   string
+		status  int
+		wantErr bool
+	}{
+		{name: "correct value", value: "9f43506d-c5f0-45e9-8871-d513e8da9018", status: http.StatusOK, wantErr: false},
+		{name: "missing value", value: "", status: http.StatusBadRequest, wantErr: true},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			req, err := http.NewRequest("GET", "localhost:8081/secret", nil)
+			if err != nil {
+				t.Fatalf("could not created request: %v", err)
+			}
+
+			vars := map[string]string{"key": tc.value}
+			req = mux.SetURLVars(req, vars)
+
+			rec := httptest.NewRecorder()
+
+			parser := &TestTemplateParser{}
+			httpGoSecret := GetGoSecret(parser)
+			httpGoSecret(rec, req)
+
+			res := rec.Result()
+			defer res.Body.Close()
+
+			if tc.wantErr {
+				if res.StatusCode != http.StatusBadRequest {
+					t.Errorf("expected status badrequest; got %v", res.Status)
+				}
+			} else {
+				if res.StatusCode != http.StatusOK {
+					t.Errorf("expected status ok; got %v", res.Status)
+				}
+				b, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					t.Fatalf("could not read response: %v", err)
+				}
+				r := string(bytes.TrimSpace(b))
+				if r == "" {
+					t.Fatalf("there is no value: %s", r)
+				}
+			}
+		})
+	}
+}
--- server/mdata.go
+++ server/mdata.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path/filepath"
+	"text/template"
 	"time"
 
 	"github.com/google/uuid"
@@ -33,4 +35,24 @@ func (s *sdata) expirationDate() time.Duration {
 		return time.Duration(s.Utime*24) * time.Hour
 	}
 	return time.Duration(s.Utime) * time.Minute
+}
+
+type TemplateParser interface {
+	ParseFiles() (*template.Template, error)
+}
+
+type DefaultTemplateParser struct{}
+
+func (r *DefaultTemplateParser) ParseFiles() (*template.Template, error) {
+	return template.ParseFiles("./ui/secret.html")
+}
+
+type TestTemplateParser struct{}
+
+func (r *TestTemplateParser) ParseFiles() (*template.Template, error) {
+	secret, err := filepath.Abs("../ui/secret.html")
+	if err != nil {
+		return nil, err
+	}
+	return template.ParseFiles(secret)
 }
